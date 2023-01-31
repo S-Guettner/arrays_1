a0623ae19d5aae5106c8956ab8e866e230dfe2b5
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-31 11:53:02 +0100

    finish

---
+++ arrays_teil_1/arrays1_5/main.js
@@ -0,0 +1,7 @@
+const songs = ["Bohemian Rhapsody", "Stairway to Heaven", "Hotel California"]
+console.log(songs);
+
+songs.push("Sweet Child O'Mine","We will rock you","we are the Champions")
+
+const totalSongs = songs 
+console.log(totalSongs);
