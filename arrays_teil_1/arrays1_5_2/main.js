b65953f47907c1d9751f2ba7e6561afc299a12b8
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-31 12:05:57 +0100

    finish

---
+++ arrays_teil_1/arrays1_5_2/main.js
@@ -0,0 +1,6 @@
+const heroUndEnemy = [["Batman","The Joker"],["Professor X","Magneto"],["Thor","Loki"]]
+console.log(heroUndEnemy);
+
+heroUndEnemy.push(["Superman","Lex Luthor"],["Wolverine","Sabretooth"],["Ghost Rider","Mephisto"])
+const moreHeroEnemy = heroUndEnemy
+console.log(moreHeroEnemy);
