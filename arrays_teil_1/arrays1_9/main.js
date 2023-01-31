9ab093a9d3e5c751c81e4e7c602d4de10bfe6088
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-31 13:07:13 +0100

    finish

---
+++ arrays_teil_1/arrays1_9/main.js
@@ -0,0 +1,14 @@
+const numbers = [23,54,75]
+console.log(numbers);
+numbers.push(100,125,150)
+console.log(numbers);
+numbers.unshift(10,12,15)
+console.log(numbers);
+numbers.pop()
+console.log(numbers);
+numbers.pop()
+console.log(numbers);
+numbers.shift()
+console.log(numbers);
+numbers.shift()
+console.log(numbers);
