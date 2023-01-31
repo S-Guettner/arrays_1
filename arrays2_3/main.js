ad5c416882e88b3c21eceb7c4e9d16a6f77dc64f
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-31 21:57:22 +0100

    working

--- arrays2_3/main.js
+++ arrays2_3/main.js
@@ -1,29 +1,21 @@
 const inputVorname = document.getElementById("inputVorname")
-/* const inputNachname = document.getElementById("inputNachname") */
 const listContainer = document.getElementById("listContainer")
 let li
-const vollerName = []
 const teilnehmerListe = []
 
 const list = (hinzufügen) => {
     if(hinzufügen){
         let name = inputVorname.value
         teilnehmerListe.push(name)
-        console.log(teilnehmerListe)
-
-
         li = document.createElement("li")
-
         listContainer.append(li)
         li.innerHTML =  teilnehmerListe[teilnehmerListe.length-1]
     }
     else {
-        li.remove("li")
+        let lastChild = document.querySelector("li:last-child")
+        lastChild.remove()
         teilnehmerListe.pop()
-        console.log(teilnehmerListe)
-        /* console.log(listContainer) */
     }
-    
     }
 
 
