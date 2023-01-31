5b7791af1675e1b3ad80487bc04cb3576f6f3bd9
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-01-31 11:57:22 +0100

    finish

--- arrays_teil_1/arrays1_5/main.js
+++ arrays_teil_1/arrays1_5/main.js
@@ -5,3 +5,10 @@ songs.push("Sweet Child O'Mine","We will rock you","we are the Champions")
 
 const totalSongs = songs 
 console.log(totalSongs);
+
+
+const besteFussballerAllerZeiten = ["Lionel Messi","Neymar","Erling Haaland","Karim Benzema","Kylian Mbappé"]
+console.log(besteFussballerAllerZeiten);
+
+besteFussballerAllerZeiten.push("Buffon","Neuer","Kahn")
+console.log(besteFussballerAllerZeiten);
